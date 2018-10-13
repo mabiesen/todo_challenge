d20aa2ad786ebec5c4458c505b7663ba782f65ff
mabiesen <mabiesen@outlook.com>
2018-10-12 19:21:07 -0500

    database creator created

--- create_database.py
+++ create_database.py
@@ -1,1 +1,13 @@
+import sqlite3
 
+def create_todo_database():
+    conn = sqlite3.connect('Todo.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE TodoList
+		 (Todo text, CreationTime text, CreationDate text)''')
+
+
+
+
+if __name__ == "__main__":
+    create_todo_database()
