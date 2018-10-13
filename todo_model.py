b740ff699329517dffb4b0ea68c30e40a07338d8
mabiesen <mabiesen@outlook.com>
2018-10-13 01:34:11 -0500

    todo_model

--- todo_model.py
+++ todo_model.py
@@ -8,18 +8,18 @@ class TodoItem():
         self.todo = todo
         self.creationdate = creationdate
         self.creationtime = creationtime
-        validate_data()
+        self.validate_data()
 
-    def validate_data():
+    def validate_data(self):
         if len(self.todo) < 4 or self.todo.upper() == "NULL":
            raise Exception("Todo item doesnt look real,\n"
 				"todo is %s"%(self.todo))
          
         now = datetime.datetime.now() 
         if self.creationdate == "":
-            self.creationdate = time.strftime(now,"%Y%M%D")
+            self.creationdate = now.strftime("%Y%m%d")
         if self.creationtime == "":
-            self.creationtime = time.strftime(now,"%h%m%s")
+            self.creationtime = now.strftime("%H%M%S")
 
         
  
