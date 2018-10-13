f79ee707357d36dc6314d1c8c79a0bf223e5025d
mabiesen <mabiesen@outlook.com>
2018-10-12 21:35:43 -0500

    work on model validations

--- todo_model.py
+++ todo_model.py
@@ -1,11 +1,21 @@
+import datetime
+import time
+
 
 class TodoItem():
     
-    def __init__(self,todo,creationdate,creationtime):
+    def __init__(self,todo,creationdate="",creationtime=""):
         self.todo = todo
-        self.creationdate = creationdate
-        self.creationtime = creationtime
-        pass
+        if creationdate == "" or creationtime == "":
+            now = datetime.datetime.now()
+            self.creationdate = time.strftime(now,"%Y%M%D")
+            self.creationtime = time.strftime(now,"%h%m%s")
+        else:
+            self.creationdate = creationdate
+            self.creationtime = creationtime
 
     def validate_data():
+        if len(self.todo) < 4:
+           raise Exception("Todo item doesnt look real, name of todo less than 4 letters")
+          
         pass
