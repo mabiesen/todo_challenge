150873fad4adf111c762c757ba42582a7a1aaf5e
mabiesen <mabiesen@outlook.com>
2018-10-13 01:18:44 -0500

    work to model

--- todo_model.py
+++ todo_model.py
@@ -4,18 +4,22 @@ import time
 
 class TodoItem():
     
-    def __init__(self,todo,creationdate="",creationtime=""):
+    def __init__(self,todo="",creationdate="",creationtime=""):
         self.todo = todo
-        if creationdate == "" or creationtime == "":
-            now = datetime.datetime.now()
-            self.creationdate = time.strftime(now,"%Y%M%D")
-            self.creationtime = time.strftime(now,"%h%m%s")
-        else:
-            self.creationdate = creationdate
-            self.creationtime = creationtime
+        self.creationdate = creationdate
+        self.creationtime = creationtime
+        validate_data()
 
     def validate_data():
-        if len(self.todo) < 4:
-           raise Exception("Todo item doesnt look real, name of todo less than 4 letters")
-          
-        pass
+        if len(self.todo) < 4 or self.todo.upper() == "NULL":
+           raise Exception("Todo item doesnt look real,\n"
+				"todo is %s"%(self.todo))
+         
+        now = datetime.datetime.now() 
+        if self.creationdate == "":
+            self.creationdate = time.strftime(now,"%Y%M%D")
+        if self.creationtime == "":
+            self.creationtime = time.strftime(now,"%h%m%s")
+
+        
+ 
