14e898f8249b705a3bd730418dffdb08521a0510
mabiesen <mabiesen@outlook.com>
2018-10-13 03:51:58 -0500

    added and confirmed check for swap files

--- RUN_TESTS.py
+++ RUN_TESTS.py
@@ -1,5 +1,10 @@
 import test_create_database
 import test_todo_model
+import subprocess
+
+output = subprocess.check_output(["ls","-la"])
+if ".swp" in output:
+    raise Exception("Not all files have been properly saved")
 
 x = "==============================================="
 print("RUNNING ALL TESTS")
