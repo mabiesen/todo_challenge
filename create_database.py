001a7aefbcb445a7557e4b5706f6987b9162b84d
mabiesen <mabiesen@outlook.com>
2018-10-12 19:39:26 -0500

    added gitignore file, created install script

--- create_database.py
+++ create_database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import subprocess
 
 def create_todo_database():
     conn = sqlite3.connect('Todo.db')
@@ -6,8 +7,13 @@ def create_todo_database():
     c.execute('''CREATE TABLE TodoList
 		 (Todo text, CreationTime text, CreationDate text)''')
 
-
+def remove_todo_database():
+    try:
+        _ = subprocess.call(['rm','Todo.db'])
+    except:
+        print("error removing database, it probably doesn't exist")
 
 
 if __name__ == "__main__":
+    remove_todo_dabase()
     create_todo_database()
---
+++ install.py
@@ -0,0 +1,4 @@
+import create_database
+
+create_database.remove_todo_database()
+create_database.create_todo_database()
