cd6984986ff30dd07f6e2d6c3362bc8e5a1050a6
mabiesen <mabiesen@outlook.com>
2018-10-12 21:09:34 -0500

    created todo model

---
+++ todo_model.py
@@ -0,0 +1,11 @@
+
+class TodoItem():
+    
+    def __init__(self,todo,creationdate,creationtime):
+        self.todo = todo
+        self.creationdate = creationdate
+        self.creationtime = creationtime
+        pass
+
+    def validate_data():
+        pass
